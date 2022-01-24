ce96b5ca0fb1a6e5a16e9a18c3957d8f195296f4
zhyj2017 <60803313+zhyj2017@users.noreply.github.com>
2022-01-24 14:17:07 +0800

    修改添加考试和架构

--- backend/etest/src/main/java/edu/fzu/etest/controller/TestController.java
+++ backend/etest/src/main/java/edu/fzu/etest/controller/TestController.java
@@ -8,6 +8,7 @@ import edu.fzu.etest.util.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @RestController
@@ -24,6 +25,14 @@ public class TestController {
     PaperQuestionService paperQuestionService;
     @Autowired
     QuestionService questionService;
+
+    @RequestMapping(value = "/AddQuestion",produces = "application/json;charset=utf-8",method= RequestMethod.POST)
+    @ResponseBody
+    public Response add(@RequestBody Question question){
+        questionService.add(question);
+        Response response = new Response(200,"添加成功",null);
+        return response;
+    }
 
     @RequestMapping(value = "/ShowQuestion",produces = "application/json;charset=utf-8",method= RequestMethod.POST)
     @ResponseBody
@@ -72,13 +81,38 @@ public class TestController {
 
     @RequestMapping(value = "/AddTest",produces = "application/json;charset=utf-8",method= RequestMethod.POST)
     @ResponseBody
-    public Response addTest(@RequestBody Test test){
+    public Response addTest(@RequestBody Map<String,Object> map){
+        Test test = new Test();
+        String tname = map.get("tname").toString();
+        String starttime = map.get("starttime").toString();
+        String endtime = map.get("endtime").toString();
+        String description = map.get("description").toString();
+        long pid = Long.valueOf(map.get("pid").toString());
+        long cid = Long.valueOf(map.get("cid").toString());
+        long aid = Long.valueOf(map.get("aid").toString());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date sdate = null;
+        Date edate = null;
+        try{
+            sdate = sdf.parse(starttime);
+            edate = sdf.parse(endtime);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        test.setTname(tname);
+        test.setStarttime(sdate);
+        test.setEndtime(edate);
+        test.setDescription(description);
+        test.setCid(cid);
+        test.setPid(pid);
+        test.setAid(aid);
         long tid = testService.add(test);
         Map<String,Object> map1 = new HashMap<>();
         map1.put("tid",tid);
         Response response = new Response(200,"",map1);
         return response;
     }
+
 
 //    @RequestMapping(value = "/Mark",produces = "application/json;charset=utf-8",method= RequestMethod.POST)
 //    @ResponseBody
