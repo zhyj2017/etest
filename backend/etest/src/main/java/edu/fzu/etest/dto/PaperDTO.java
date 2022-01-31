02dfcb7cfa9dd4bdcab2a6f707a181ce268773cb
zhyj2017 <60803313+zhyj2017@users.noreply.github.com>
2022-01-31 14:48:09 +0800

    删除无用代码

--- backend/etest/src/main/java/edu/fzu/etest/dto/PaperDTO.java
+++
@@ -1,4 +0,0 @@
-package edu.fzu.etest.dto;
-
-public class PaperDTO {
-}
--- backend/etest/src/main/java/edu/fzu/etest/mapper/Student_ClassMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.fzu.etest.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import edu.fzu.etest.bean.Student_Class;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface Student_ClassMapper extends BaseMapper<Student_Class> {
-}
